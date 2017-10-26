89012135fc85fb1016c54b3103ab6931d9e296fb
BuaaAlen <dxz1215225@buaa.edu.cn>
2017-10-26 16:44:45 +0800

    push dist

---
+++ frontend/dist/static/js/index.js
@@ -0,0 +1,11 @@
+function clearNavActive()
+{
+	$("#home").removeClass("active");
+	$("#course").removeClass("active");
+	$("#contact").removeClass("active");
+}
+
+function a()
+{
+	alert("按钮调用成功!");
+}
