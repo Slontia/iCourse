044ced0f89ae4058ea9694b738d25fec443c251a
BuaaAlen <dxz1215225@buaa.edu.cn>
2017-10-26 19:48:01 +0800

    完成了登录/注册按钮的模态窗形态

--- frontend/dist/static/js/index.js
+++ frontend/dist/static/js/index.js
@@ -9,3 +9,23 @@ function a()
 {
 	alert("按钮调用成功!");
 }
+
+function loginModal()
+{
+	$("#regBtn").removeClass("btn-primary");
+	$("#regBtn").addClass("btn-default");
+	$("#logBtn").removeClass("btn-default");
+	$("#logBtn").addClass("btn-primary");
+	$("#regModal").hide();
+    $("#logModal").show();
+}
+
+function registerModal()
+{
+	$("#logBtn").removeClass("btn-primary");
+	$("#logBtn").addClass("btn-default");
+    $("#regBtn").removeClass("btn-default");
+	$("#regBtn").addClass("btn-primary");
+	$("#logModal").hide();
+    $("#regModal").show();	
+}
