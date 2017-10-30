e97167b0f3922df118d39d15312c0e4100286a20
BuaaAlen <dxz1215225@buaa.edu.cn>
2017-10-30 19:54:46 +0800

    创建了资源页面

---
+++ frontend/src/components/course/resource.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="resource">
+    <Header></Header>
+    <div style="height:500px;width:500px;background-color:red;"></div>
+  </div>
+</template>
+
+
+<script type="text/javascript">
+/* eslint-disable brace-style */
+import Header from '../general/Header'
+export default {
+  name: 'resource',
+  components: { Header },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Course from '@/components/course/Course'
+import Resource from '@/components/course/resource'
 import Personal from '@/components/Personal'
 import PersonalData from '@/components/PersonalData'
 import ElementUI from 'element-ui'
@@ -26,6 +27,11 @@ export default new Router({
       path: '/course',
       name: 'course',
       component: Course
+    },
+    {
+      path: '/resource',
+      name: 'resource',
+      component: Resource
     },
     {
       path: '/personal',
