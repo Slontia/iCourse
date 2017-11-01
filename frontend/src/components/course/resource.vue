e95e8474640e19a230546a61cc10bbdad36d9c91
BuaaAlen <dxz1215225@buaa.edu.cn>
2017-11-01 21:38:06 +0800

    add resource dialog

--- frontend/src/components/course/resource.vue
+++ frontend/src/components/course/resource.vue
@@ -53,7 +53,7 @@
       </el-row>
       <el-row style="margin:20px 0px 0px 0px;">
         <el-col :span="6" :offset="2">          
-        <el-row v-for="item in resourcesData" v-if="item.col1" class="hoverChange">
+        <el-row v-for="item in resourcesData" v-if="item.col1" class="hoverChange"  @click.native="dialogVisible=true">
           <el-row style="margin:20px 0px 0px 0px;" >
             <el-col :span="8" :offset="1">
               <img :src="zipImg" style="height:100px;"></img>
@@ -90,13 +90,12 @@
             </el-col>
           </el-row>
           <el-row style="margin:5px 0px 0px 0px;height:1px;background-color:black;"></el-row>
-        </el-row> 
-        
+        </el-row>        
         </el-col>
          
 
         <el-col :span="6" :offset="1">
-          <el-row v-for="item in resourcesData" v-if="item.col2" class="hoverChange">
+          <el-row v-for="item in resourcesData" v-if="item.col2" class="hoverChange"  @click.native="dialogVisible=true">
           <el-row style="margin:20px 0px 0px 0px;" >
             <el-col :span="8" :offset="1">
               <img :src="zipImg" style="height:100px;"></img>
@@ -136,7 +135,7 @@
         </el-row>
         </el-col>
         <el-col :span="6" :offset="1">
-          <el-row v-for="item in resourcesData" v-if="item.col3" class="hoverChange">
+          <el-row v-for="item in resourcesData" v-if="item.col3" class="hoverChange" @click.native="dialogVisible=true">
           <el-row style="margin:20px 0px 0px 0px;" >
             <el-col :span="8" :offset="1">
               <img :src="zipImg" style="height:100px;"></img>
@@ -177,6 +176,94 @@
         </el-col>
       </el-row>
     </el-row>
+
+    <!-- 资源详细信息窗口 -->
+    <el-dialog title="资源信息" :visible.sync="dialogVisible" size="tiny">
+      <el-row>
+        <el-col :span="24">
+          <div id="dialogTitle">全部资源</div>
+        </el-col>
+      </el-row>
+      <el-row style="margin:15px 0px 0px 0px;">
+        <el-col :span="16" :offset="1">
+          <el-row>
+            <el-col :span="8">
+              上传者:
+            </el-col>
+            <el-col :span="16">
+              <img :src="zipImg" style="height:18px;"></img>
+              <a href="">果冻</a>
+            </el-col>
+          </el-row>
+          <el-row style="margin:10px 0px 0px 0px;">
+            <el-col :span="8">
+              上传时间:
+            </el-col>
+            <el-col :span="16">
+              2017-3-26
+            </el-col>
+          </el-row>
+          <el-row style="margin:10px 0px 0px 0px;">
+            <el-col :span="8">
+              资源大小:
+            </el-col>
+            <el-col :span="16">
+              98.6MB
+            </el-col>
+          </el-row>
+          <el-row style="margin:10px 0px 0px 0px;">
+            <el-col :span="24">
+              简介:
+            </el-col>
+          </el-row>
+          <el-row style="margin:10px 0px 0px 0px;">
+            <el-col :span="24">
+              里面整合了1~13章课件，全部是pdf格式
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="7">
+          <el-row style="text-align:center;">
+            <img :src="zipImg" style="height:120px;"></img>
+          </el-row>
+          <el-row style="text-align:center;">
+            <el-button type="primary">点击下载</el-button>
+          </el-row>
+        </el-col>
+      </el-row>
+      <el-row style="margin:10px 0px 0px 0px;">
+        <el-col :span="2" :offset="1">标签:</el-col>
+        <el-col :span="3">课件:</el-col>
+        <el-col :span="2" :offset="11"><a href="">好评</a></el-col>
+        <el-col :span="1">21</el-col>
+        <el-col :span="2" :offset="1"><a href="">差评</a></el-col>
+        <el-col :span="1">2</el-col>
+      </el-row>
+      <el-row style="margin:10px 0px 0px 0px;">
+        <el-col :span="1" :offset="1"><i class="el-icon-star-off"></i></el-col>
+        <el-col :span="1">23</el-col>
+        <el-col :span="1" :offset="1"><i class="el-icon-arrow-down"></i></el-col>
+        <el-col :span="1">656</el-col>
+      </el-row>
+      <el-row style="margin:20px 0px 0px 0px;">
+        <el-col :span="4" :offset="1" style="height:20px;font-weight:bold;font-size:20px;">全部评论</el-col>
+        <el-col :span="1" :offset="1" style="height:20px;line-height:20px;"><i class="el-icon-message"></i></el-col>
+        <el-col :span="1" style="height:20px;line-height:20px;">0</el-col>
+        <el-col :span="4" :offset="12" style="height:20px;line-height:20px;"><a href="">我要评论</a></el-col>        
+      </el-row>
+      <el-row style="margin:5px 0px 0px 0px;">
+        <el-col :span="22" :offset="1">
+          <el-row style="background-color:black;height:3px;"></el-row>
+        </el-col>
+      </el-row>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible=false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
+
+
   </div>
 </template>
 
@@ -189,6 +276,7 @@ export default {
   components: { Header },
   data () {
     return {
+      dialogVisible: false,
       course: '软件工程基础',
       options: [
         {
@@ -205,42 +293,6 @@ export default {
       sortMode: '按上传时间排序',
       zipImg: ZipImg,
       resourcesData: [
-        {
-          col1: false,
-          col2: true,
-          col3: false,
-          name: '全部课件.zip',
-          intro: '里面整合了1~13章课件，全部是pdf格式',
-          collections: 23,
-          downloads: 56,
-          messages: 2,
-          author: '果冻',
-          time: '2017-3-26'
-        },
-        {
-          col1: false,
-          col2: false,
-          col3: true,
-          name: '全部课件.zip',
-          intro: '里面整合了1~13章课件，全部是pdf格式',
-          collections: 23,
-          downloads: 56,
-          messages: 2,
-          author: '果冻',
-          time: '2017-3-26'
-        },
-        {
-          col1: true,
-          col2: false,
-          col3: false,
-          name: '全部课件.zip',
-          intro: '里面整合了1~13章课件，全部是pdf格式',
-          collections: 23,
-          downloads: 56,
-          messages: 2,
-          author: '果冻',
-          time: '2017-3-26'
-        },
         {
           col1: false,
           col2: true,
@@ -313,6 +365,13 @@ export default {
     margin: 10px 0px 0px 0px;
   }
   .hoverChange:hover {
-    background-color:#fff0f5;
+    background-color:#7fffd4;
+  }
+  #dialogTitle {
+    height: 30px;
+    line-height: 30px;
+    font-size: 25px;
+    font-weight: bold;
+    text-align: center;
   }
 </style>
