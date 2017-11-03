dfc3a0c91aa5f99228c57d9786dd24959d17ed01
slontia <654867229@qq.com>
2017-11-03 10:43:45 +0800

    remove json sender

--- backend/interface.py
+++ backend/interface.py
@@ -16,13 +16,6 @@ def college_course_list(col_id):
     result = Course.objects.filter(college_id=col_id).values_list("id", flat=True)
     return list(result)
 
-def college_course_list_sender(request):
-	if request.method == "POST":
-		result = college_course_list(request.POST.get('college_id'))
-		return HttpResponse(json.dumps({
-		    "course_id_list" : result
-		}))
-
 # 通过某课程类别获取课程列表
 # REQUIRES: type(cla_id) == <class 'int'>
 # MODIFIES: None
@@ -31,13 +24,6 @@ def college_course_list_sender(request):
 def classification_course_list(cla_id):
     result = Course.objects.filter(class_id=cla_id).values_list("id", flat=True)
     return list(result)
-
-def classification_course_list_sender(request):
-	if request.method == "POST":
-		result = classification_course_list(request.POST.get('course_class'))
-		return HttpResponse(json.dumps({
-		    "course_id_list" : result
-		}))
 
 # 查询课程详细信息
 # REQUIRES: type(cou_id) == <class 'int'>
@@ -50,22 +36,6 @@ def course_information(cou_id):
         return {}
     result = result.values()[0] 
     return result
-
-
-def course_information_sender(request):
-    if request.method == "POST":
-    	result = course_information(request.POST.get('course_id'))
-        if result == {}:
-        	return HttpResponse(json.dumps({}))
-        return HttpResponse(json.dumps({
-            "name" : result.name,
-            "hours" : result.hours,
-            "credit" : result.credit,
-            "teacher" : null,
-            "college" : null, # College类需提供college_id属性
-            "intro" : null,
-            "picture" : null
-        }))
 
 
 # 查询课程贡献度列表
@@ -94,13 +64,6 @@ def course_contribution_list(cou_id):
     result = sorted(result, key=lambda x:x[1], reverse=True)
     return result
 
-def course_contribution_list_sender(request):
-    if request.method == "POST":
-    	result = course_contribution_list(request.POST.get('course_id'))
-    	return HttpResponse(json.dumps({
-    		"list" : result
-    	}))
-
 # 查询资源信息
 # REQUIRES: type(resource_id) == <class 'int'>
 # MODIFIES: None
@@ -112,22 +75,6 @@ def resource_information(resource_id):
         return {}
     result = result.values()[0]
     return result
-
-def resource_information_sender(request):
-	if request.method == "POST":
-		result = resource_information(request.POST.get('resource_id'))
-		return HttpResponse(json.dumps({
-			"name" : result.name,
-			"size" : result.size,
-		    "link" : result.link,
-		    "intro" : result.intro,
-		    "upload_time" : result.upload_time,
-		    "upload_user_id" : result.upload_user_id,
-		    "download_num" : null,
-		    "like_num" : null,
-		    "eva_pos_num" : null,
-		    "eva_neg_num" : null
-		}))
 
 # 查询个人信息
 # REQUIRES: type(username) == <class 'string'>
