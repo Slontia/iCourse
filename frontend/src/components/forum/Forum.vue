edef90bff2f3494d44a7df2531cccb8dd628dc6f
Aria-K-Alethia <xindetai@qq.com>
2017-11-28 21:26:01 +0800

    new:forum page almost complete;construct a thread page

---
+++ frontend/src/components/forum/Forum.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id='forum' class='container'>
+    <Header></Header>
+    <el-row>
+      <el-col :span="24">
+        <el-button type="primary" icon="d-arrow-left" style="margin:20px 0px 0px 20px" @click="return_course_button_clicked">返回课程页面
+        </el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="24">
+        <p class="title">
+          {{ course_name }}-讨论版
+        </p>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="9" :offset="3">
+        <el-button-group>
+          <template v-for="term in search_terms">
+            <el-button type="plain" class="button_group_static">{{ term.text }}</el-button>
+          </template>
+        </el-button-group>
+      </el-col>
+      <el-col :span="6" :offset="2">
+        <el-input type="text" v-model = "search_text" placeholder="在当前条目下搜索" @keydown.enter.native.prevent="search_button_clicked" icon="search" :on-icon-click="search_button_clicked"></el-input>
+      </el-col>
+    </el-row>
+    <template v-for="thread in current_threads">
+      <el-row type="flex" justify="center" class="thread_container">
+        <el-col :span="4">
+          <el-row>
+            <el-col :span="8">
+              <p class="thread_info_num" style="color:#58B7FF">{{ thread.agree_num }}</p>
+            </el-col>
+            <el-col :span="8">
+              <p class="thread_info_num" style="color:rgb(80,80,80)">{{ thread.follow_num }}</p>
+            </el-col>
+            <el-col :span="8">
+              <p class="thread_info_num" style="color:grey">{{ thread.read_num }}</p>
+            </el-col>
+          </el-row>
+          <el-row>
+            <el-col :span="8">
+              <p class="thread_info_term" style="color:#58B7FF">赞同</p>
+            </el-col>
+            <el-col :span="8">
+              <p class="thread_info_term" style="color:rgb(80,80,80)">跟帖</p>
+            </el-col>
+            <el-col :span="8">
+              <p class="thread_info_term" style="color:grey">浏览</p>
+            </el-col>
+          </el-row>
+        </el-col>
+        <el-col :span="14" :offset="1">
+          <el-row style="padding-bottom: 10px;">
+            <el-button type="text" class="title_button">
+            <p>
+              <span>{{ thread.type }} ·</span>
+              <span>{{ thread.title }}</span>
+            </p>
+          </el-button>
+          </el-row>
+          <el-row style="padding-bottom: 10px;">
+            <p style="color: grey"> {{ thread.description }} </p>
+          </el-row>
+          <el-row>
+            <p style="float: right;color:grey"> {{ thread.user_name }} 最后编辑于 {{ thread.time }}</p>
+          </el-row>
+        </el-col>
+      </el-row>
+      <center><hr width="80%" style="" /></center>
+    </template>
+    <el-row type="flex" justify="center">
+      <el-col :span="18">
+      <center><el-pagination layout="prev,pager,next" @current-change="handle_current_change" :page-size="page_size" :total="total_thread" :current-page.sync="current_page" style="margin-top: 40px"></el-pagination>
+      </center>
+    </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script type="text/javascript">
+/* eslint-disable brace-style */
+/* eslint-disable camelcase */
+/* eslint-disable space-infix-ops */
+import Header from '../general/Header'
+export default {
+  name: 'Forum',
+  components: { Header },
+  data () {
+    return {
+      course_name: '软件工程',
+      total_thread: 0,
+      page_size: 10,
+      current_page: 1,
+      current_threads: [
+        { agree_num: 12, follow_num: 5, read_num: 100, type: '学习心得', title: '关于最大团问题的典型解法', description: '很惭愧，一点微小的工作', user_name: '果冻', time: '2017-11-29' },
+        { agree_num: 11, follow_num: 3, read_num: 110, type: '问题讨论', title: '如何评论最近上线的BUAA-iCourse?', description: '如题', user_name: 'Aletheia', time: '2017-11-29' }
+      ],
+      threads: [],
+      search_terms: [
+        { text: '全部' },
+        { text: '学习心得' },
+        { text: '问题讨论' },
+        { text: '其他' }
+      ],
+      thread_terms: [ '赞同', '跟帖', '浏览' ]
+
+    }
+  },
+  methods: {
+    return_course_button_clicked: function () {
+      this.$router.push({ path: '/course/page/'+this.$route.params.course_id+'/' })
+    },
+    search_button_clicked: function () {
+      this.$message({
+        showClose: true,
+        message: '尚未开放，敬请期待'
+      })
+    },
+    handle_current_change: function (value) {
+
+    }
+  }
+}
+</script>
+
+<style type="text/css" scoped>
+  .container{
+    font-family: Microsoft Yahei;
+  }
+  .title{
+    text-align: center;
+    font-size: 40px;
+    padding-bottom: 30px;
+  }
+  .button_group_static{
+    width: 100px;
+    text-align: center;
+  }
+  .thread_info_num{
+    text-align: center;
+    font-size: 24px;
+    padding-bottom: 10px;
+    padding-top: 0px;
+    margin-top: 0px;
+  }
+  .thread_info_term{
+    text-align: center;
+    font-size: 24px;
+  }
+  .title_button{
+    padding: 0px 0px 0px 0px;
+    margin: 4px 0px 0px 0px;
+    color:black;
+    font-size: 24px;
+  }
+  .title_button:hover{
+    color: #58B7FF;
+    text-decoration: underline;
+  }
+  .thread_container{
+    margin-top: 30px;
+    height: 100px;
+  }
+</style>
---
+++ frontend/src/components/forum/Thread.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="thread">
+    <Header></Header>
+  </div>
+</template>
+
+<script type="text/javascript">
+/* eslint-disable brace-style */
+/* eslint-disable camelcase */
+/* eslint-disable space-infix-ops */
+import Header from '../general/Header'
+export default {
+  name: 'Thread',
+  components: { Header },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style type="text/css" scpoed>
+</style>
